d3cd70af9bb8c4c841891b8a784a9e5edffd0cf8
Julia Plichta <juliaplichta0@gmail.com>
2020-06-22 15:24:24 +0200

    Add flame boundary disorder detection based on convex hull

--- main.cpp
+++ main.cpp
@@ -14,34 +14,22 @@ int main() {
 
 	int choice = 0;
 
-	cout << "0. Camera" << endl;
-	cout << "1. Video file 1" << endl;
-	cout << "2. Video file 2" << endl;
-	cout << "3. Video file 3" << endl;
-	cout << "4. Video file 4" << endl;
+	cout << "1. Camera" << endl;
+	cout << "2. Video from your computer" << endl;
 	cout << "Choose source video:" << endl;
 	cin >> choice;
 
 	switch (choice) 
 	{
-		case 0: {
+		case 1: {
 			cap.open(0);
 			break;
 		}
-		case 1: {
-			cap.open("firevideo1.mp4");
-			break;
-		}
 		case 2: {
-			cap.open("firevideo2.mp4");
-			break;
-		}
-		case 3: {
-			cap.open("firevideo3.mp4");
-			break;
-		}
-		case 4: {
-			cap.open("firevideo4.mp4");
+			cout << "Specify full pathname:" << endl;
+			string pathname;
+			cin >> pathname;
+			cap.open(pathname);
 			break;
 		}
 		default: {
@@ -63,6 +51,12 @@ int main() {
 	int max_thresh_value = 255;
 
 	cap >> frame;
+
+	Scalar yellow = Scalar(0, 250, 250);
+	Scalar blue = Scalar(255, 60, 0);
+	Scalar red = Scalar(0, 0, 255);
+
+	int is_fire = 0;
 
 	while (true)
 	{
@@ -90,19 +84,72 @@ int main() {
 		//GaussianBlur(frame_motion, frame_motion, Size(15, 15), 0);
 		cvtColor(frame_motion, frame_hsv, COLOR_BGR2HSV);
 
-		Mat frame_fire;
+		Mat frame_fire, frame_fire_gray;
 		inRange(frame_hsv, Scalar(0, 0, 160), Scalar(30, 255, 255), mask1);
 		inRange(frame_hsv, Scalar(160, 50, 160), Scalar(180, 255, 255), mask2);
 
 		Mat1b mask = mask1 | mask2;
 
 		bitwise_and(frame, frame_hsv, frame_fire, mask);
+		
+		cvtColor(frame_fire, frame_fire_gray, COLOR_HSV2BGR);
+		cvtColor(frame_fire_gray, frame_fire_gray, COLOR_BGR2GRAY);
+
+		Mat frame_fire_binary;
+		blur(frame_fire_gray, frame_fire_binary, Size(3, 3));
+		threshold(frame_fire_binary, frame_fire_binary, 50, 255, THRESH_BINARY);
+
+		vector<vector<Point>> contours;
+		findContours(frame_fire_binary, contours, RETR_TREE, CHAIN_APPROX_SIMPLE);
+
+		vector<vector<Point>> hull(contours.size());
+		vector<double> contours_perimeter(contours.size());
+		vector<double> hull_perimeter(contours.size());
+		vector<double> ratio(contours.size());
+
+		for (size_t i = 0; i < contours.size(); i++)
+		{
+			convexHull(contours[i], hull[i]);
+			contours_perimeter[i] = contours[i].size();
+			hull_perimeter[i] = hull[i].size();
+			ratio[i] = hull_perimeter[i] / contours_perimeter[i];
+		}
+
+		for (size_t i = 0; i < contours.size(); i++)
+		{
+			if (ratio[i] <= 0.6) {
+				drawContours(frame, contours, (int)i, blue, -1);
+				is_fire = 2;
+			}
+			else if (ratio[i] > 0.6 && ratio[i] < 0.8) {
+				drawContours(frame, contours, (int)i, yellow, -1);
+				if (is_fire == 0)
+					is_fire = 1;
+			}
+			//else
+			//	putText(frame, "WARNING!!!", Point(10, 30), FONT_HERSHEY_PLAIN, 2, yellow, 2);
+		}
+
+		if (is_fire == 2) {
+			putText(frame, "FIRE!!!", Point(10, 30), FONT_HERSHEY_PLAIN, 2, red, 2);
+			
+		}
+
+		if (is_fire == 1)
+			putText(frame, "WARNING!!!", Point(10, 30), FONT_HERSHEY_PLAIN, 2, yellow, 2);
+		is_fire = 0;
+
+		//int non_zero = countNonZero(frame_fire_gray);
+		//if (non_zero > 1) {
+		//	if (non_zero > 30)
+		//		putText(frame, "FIRE!!!", Point(10, 30), FONT_HERSHEY_PLAIN, 2, blue, 2);
+		//		//fire;
+		//	else 
+		//		putText(frame, "WARNING!!!", Point(10, 30), FONT_HERSHEY_PLAIN, 2, yellow, 2);
+		//		//warning
+		//}
 
 		imshow("Frame", frame);
-		imshow("Frame Diff", frame_diff);
-		imshow("Frame Thresh", frame_thresh);
-		imshow("Frame Thresh Dilate", frame_thresh_dilate);
-		imshow("Frame Motion", frame_motion);
 		imshow("Frame Fire", frame_fire);
 
 		waitKey(15);
